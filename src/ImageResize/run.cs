18a416395c1fbcac4b19cadaf16190b87a2187ff
Jef King <jefkin@microsoft.com>
2019-02-21 10:00:51 -0800

    fixing up dir

--- src/ImageResize/run.cs
+++ src/ImageResize/run.cs
@@ -1,24 +1,29 @@
-#r "System.Drawing"
-
 using System;
+using System.IO;
 using System.Drawing;
 using ImageProcessor;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
 
-private static readonly Size size = new Size(EnvAsInt("ImageResize-Width"), EnvAsInt("ImageResize-Height"));
-
-[FunctionName("ImageResize")]
-[return: Blob("thumbnails/{name}")]
-public static void Run(Stream original, Stream resized)
+namespace serverlesslibrary
 {
-    using (var imageFactory = new ImageFactory())
+    private static readonly Size size = new Size(EnvAsInt("ImageResize-Width"), EnvAsInt("ImageResize-Height"));
+
+    [FunctionName("ImageResize")]
+    [return: Blob("thumbnails/{name}")]
+    public static void Run([BlobTrigger("images/{name}", Connection = "ImageRepository")]Stream myBlob, string name)
     {
-        imageFactory
-            .Load(original)
-            .Resize(size)
-            .Save(resized);
+        using (var imageFactory = new ImageFactory())
+        {
+            imageFactory
+                .Load(original)
+                .Resize(size)
+                .Save(resized);
+        }
     }
+
+
+    private static int EnvAsInt(string name) => int.Parse(Env(name));
+    private static string Env(string name) => System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
 }
-
-
-private static int EnvAsInt(string name) => int.Parse(Env(name));
-private static string Env(string name) => System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
